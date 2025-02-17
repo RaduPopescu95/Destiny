e027d872067d62b10b1075d43a709d4f05e486f5
Adrian <radu.p1995@yahoo.com>
2025-02-17 11:54:19 +0200

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import Context from "@/context/Context";
 import { AuthProvider } from "@/context/AuthContext";
-import { LanguageProvider } from "@/context/LanguageContext";
+
 import Cookies from "js-cookie";
 import { GoogleTagManager } from "@next/third-parties/google";
 
